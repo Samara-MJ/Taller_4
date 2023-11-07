f8d5e48e15bdc7426346c56d2928fa32bb8817ec
Samara-MJ <s.martinezj@uniandes.edu.co>
2023-11-06 22:09:50 -0500

    Jlabel

--- Taller4_LightsOut_esqueleto/src/presentacion/Fprincipal.java
+++ Taller4_LightsOut_esqueleto/src/presentacion/Fprincipal.java
@@ -1,9 +1,13 @@
 package presentacion;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.Icon;
+
 
 public class Fprincipal extends JFrame {
 	public Fprincipal()
@@ -12,14 +16,22 @@ public class Fprincipal extends JFrame {
 		setLayout(new BorderLayout());
 		
 		JLabel derecha = new JLabel("derecha");
+		derecha.setSize(200,500);
 		derecha.setBackground(Color.GRAY);
 		derecha.setOpaque(true);
-		JLabel centro = new JLabel("centro");
-		centro.setBackground(Color.lightGray);
+		JLabel centro = new JLabel();
+		centro.setBackground(Color.YELLOW);
 		centro.setOpaque(true);
+		centro.setSize(100,100);
+		ImageIcon imagen = new ImageIcon("data/luz.png");
+		Icon icono = new ImageIcon(imagen.getImage().getScaledInstance(centro.getWidth(), centro.getHeight(), Image.SCALE_DEFAULT));
+		centro.setIcon(icono);
+		
 		
 		add(derecha, BorderLayout.EAST);
 		add(centro, BorderLayout.CENTER);
+		
+		
 		
 		setTitle("LightsOut");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
